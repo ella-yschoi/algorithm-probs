bda5f9a3f711f11820748d9cb47c758c55f30d71
Ella Yeonsu Choi <happymondaysu@gmail.com>
2024-01-12 14:28:52 +0900

    [level Lv.2] Title: JadenCase 문자열 만들기, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/Lv.2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/Lv.2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,15 +1,16 @@
 function solution(s) {
-    // s: 문자열
-    // return: JadenCase로 바꾼 문자열
-    // 일단 모두 소문자로 바꿔줌
-    // 공백이 있다면
-    // 공백 바로 뒤의 첫번째 글자가 소문자라면 대문자로 바꾸기 (숫자라면 X)
-    const words = s.toLowerCase().split(' ');
+    // 문자열 s
+    // 모두 소문자로 바꿔주고 공백 포함 쪼갠걸 words로 선언
+    const words = s.toLowerCase().split(' ')
+    // for문 순회해서 각 글자 탐색하고
     for (let i = 0; i < words.length; i++) {
+        // 만약 문자열 길이가 0보다 크면
         if (words[i].length > 0) {
-            words[i] = words[i][0].toUpperCase() + words[i].substring(1);
+            // 첫 번째 글자를 대문자로, 나머지 글자는 그대로 리턴
+            words[i] = words[i][0].toUpperCase() + words[i].substring(1);;
         }
-    }
+    }    
+    // JadenCase로 바꾼 answer 리턴
     const answer = words.join(' ');
     return answer;
 }
