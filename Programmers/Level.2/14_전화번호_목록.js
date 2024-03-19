dc7ef1817797d631c489ba0c101ea1ccde02ca2b
Ella Yeonsu Choi <happymondaysu@gmail.com>
2024-03-20 00:35:41 +0900

    docs: Add 14_전화번호_목록.js of Lv.2

---
+++ Programmers/Level.2/14_전화번호_목록.js
@@ -0,0 +1,42 @@
+// 14. 전화번호 목록
+// 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=javascript
+
+// 나의 풀이
+function solution(phone_book) {
+  // 전화번호부 정렬
+  phone_book.sort();
+  
+  // indexOf() 사용해 인접한 번호끼리 접두어인지 확인
+  for (let i = 0; i < phone_book.length - 1; i++) {
+    if (phone_book[i + 1].indexOf(phone_book[i]) === 0) {
+      return false; // 접두어 있음
+    }
+  }
+
+  return true; // 접두어 없음
+}
+
+// 다른 풀이
+function solution(phone_book) {
+  // 전화번호부 정렬
+  phone_book.sort();
+
+  // startsWith() 사용해 인접한 번호끼리 접두어인지 확인
+  for (let i = 0; i < phone_book.length - 1; i++) {
+    if (phone_book[i + 1].startsWith(phone_book[i])) {
+      return false; // 접두어 있음
+    }
+  }
+
+  return true; // 접두어 없음
+}
+
+// TIL
+// 문제 시작 시 sort()를 해주는 이유
+  // 전화번호 목록을 사전 순으로 정렬하여, 접두어 관계에 있는 전화번호들이 서로 인접하게 배치되도록 하기 위함이다.
+  // 이 정렬 과정을 거치면, 전화번호 목록을 순회하면서 바로 다음 번호와만 비교하면 접두어 관계를 쉽게 확인할 수 있다.
+  // e.g. 정렬되지 않은 목록 ["12345", "123", "23456"]에서 "123"이 "12345"의 접두어인지 확인하려면, "123"과 목록의 모든 다른 번호를 비교해야 한다.
+  // 하지만 목록을 정렬하여 ["123", "12345", "23456"]로 만들면, "123"과 바로 다음 번호인 "12345"만 비교하면 된다.
+  // 이렇게 정렬을 사용하면 알고리즘의 효율성을 크게 향상시킬 수 있다.
+  // 즉, 각 번호를 한 번씩만 검사하면 되므로, 시간 복잡도가 O(NlogN)으로 감소하며, 이는 전화번호 목록의 길이가 큰 경우 더 중요하다.
+  // => 결론: 시간 복잡도 감소를 위해 정렬이 필요하다면 sort() 해주자!
