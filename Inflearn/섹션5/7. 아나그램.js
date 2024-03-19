e7697774962e6e370e649ec973ca1aa0e2e5c2c3
Ella Yeonsu Choi <happymondaysu@gmail.com>
2024-03-19 22:13:40 +0900

    docs: Update 7. 아나그램.js of Section 5

--- Inflearn/섹션5/7. 아나그램.js
+++ Inflearn/섹션5/7. 아나그램.js
@@ -1,16 +1,37 @@
-function solution(str1, str2){
-  let answer="YES"; 
-  let sH = new Map();
-  for(let x of str1){
-    if(sH.has(x)) sH.set(x, sH.get(x)+1);
-    else sH.set(x, 1);
+// 나의 풀이
+  // 각 문자의 출현 횟수를 계산하며 상쇄시키는 방식으로 접근
+  // 순서는 다르지만 구성은 일치하는지 판별 (대소문자 구분됨)
+  // -> 각 값을 Map 객체 생성 후 has
+  // 두 개 맞으면 "YES", 아니면 "NO" 출력
+
+function solution(str1, str2) {
+  // 일단 두 문자열 길이가 다르면 아나그램 아님
+  if (str1.length !== str2.length) {
+    return "NO";
   }
-  for(let x of str2){
-    if(!sH.has(x) || sH.get(x)==0) return "NO";
-    sH.set(x, sH.get(x)-1);
+
+  // Map 객체 생성
+  let map = new Map();
+
+  // str1의 문자 카운트 증가시키기
+  for (let char of str1) {
+    if (map.has(char)) {
+      map.set(char, map.get(char) + 1);
+    } else {
+      map.set(char, 1);
+    }
   }
-  return answer;
+
+  // str2의 문자 카운트 감소시키기
+  for (let char of str2) {
+    if (!map.has(char) || map.get(char) === 0) {
+      return "NO"; // 겹치는 문자가 없거나, (str1은 있는데 str2의) 카운트가 0이면 아나그램 아님
+    }
+    map.set(char, map.get(char) - 1);
+  }
+
+  return "YES";
 }
 
-let a="AbaAeCe";
-let b="baeeACA";
+console.log(solution("AbaAeCe", "baeeACA")); // YES
+console.log(solution("hello", "billion")); // NO
