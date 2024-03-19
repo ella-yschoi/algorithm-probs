71b1777fd2e03df863979d7256f138f708d7de53
Ella Yeonsu Choi <happymondaysu@gmail.com>
2024-03-19 09:22:37 +0900

    docs: Update 4. 1부터 N까지의 합.js of Section 1

--- Inflearn/섹션1/4. 1부터 N까지의 합.js
+++ Inflearn/섹션1/4. 1부터 N까지의 합.js
@@ -1,10 +1,12 @@
 function solution(n){
   let answer = 0;
-  for(let i=1; i<=n; i++){
-    answer=answer+i;
+  let array = [];
+
+  for (let i = 1; i < n; i++) {
+    answer = answer + i;
   }
-  
+
   return answer;
 }
 
-console.log(solution(10));
+console.log(solution(10)); // 45
