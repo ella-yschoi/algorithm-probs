e385145155b5045a4cac6c1a8451676dec8c13e6
Ella Yeonsu Choi <happymondaysu@gmail.com>
2024-03-20 17:13:59 +0900

    docs: Update 3. Special Sort.js of Section 7

--- Inflearn/섹션7/3. Special Sort.js
+++ Inflearn/섹션7/3. Special Sort.js
@@ -1,31 +1,50 @@
-// 다른 풀이 1
-function solution(arr){
-  let answer=arr;
-  for(let i=0; i<arr.length-1; i++){
-    for(let j=0; j<arr.length-i-1; j++){
-      if(arr[j]>0 && arr[j+1]<0){
-        [arr[j], arr[j+1]]=[arr[j+1], arr[j]];
+// 나의 풀이 -> 틀린 풀이!
+// 음의 정수는 앞, 양의 정수가 뒤
+// 단, 양의 정수와 음의 정수 순서는 변함 없어야 함
+function solution(arr1) {
+  let answer = arr1;
+  for (let i = 0; i < arr1.length; i++) {
+    let idx = i;
+    for (let j = i + 1; j < arr1.length; j++) {
+      if (arr1[j] < 0) idx = j;
+    }
+    [arr1[i], arr1[idx]] = [arr1[idx], arr1[i]];
+  }
+  return answer;
+}
+
+let arr1 = [1, 2, 3, -3, -2, 5, 6, -6];
+console.log(solution(arr1)); // -6, -2, -3, 3, 2, 5, 6, 1
+
+
+// 다른 풀이 1 -> 버블 정렬 사용! 이웃한 것끼리만 바꾸므로 순서가 바뀌지 않음
+function solution(arr2) {
+  let answer = arr2;
+  for (let i = 0; i < arr2.length - 1; i++) {
+    for (let j = 0; j <arr2.length - i - 1; j++) {
+      if (arr2[j] > 0 && arr2[j + 1] < 0){
+        [arr2[j], arr2[j + 1]] = [arr2[j + 1], arr2[j]];
       }
     }   
   }
   return answer;
 }
 
-let arr1=[1, 2, 3, -3, -2, 5, 6, -6];
-console.log(solution(arr1));
+let arr2 = [1, 2, 3, -3, -2, 5, 6, -6];
+console.log(solution(arr2)); // -3 -2 -6 1 2 3 5 6
 
 
 // 다른 풀이 2
-function solution(arr){
+function solution(arr3){
   let answer=[];
-  for(let x of arr){
-    if(x<0) answer.push(x);
+  for (let x of arr3) {
+    if(x < 0) answer.push(x);
   }
-  for(let x of arr){
-    if(x>0) answer.push(x);
+  for (let x of arr3) {
+    if(x > 0) answer.push(x);
   }
   return answer;
 }
 
-let arr2=[1, 2, 3, -3, -2, 5, 6, -6];
-console.log(solution(arr2));
+let arr3=[1, 2, 3, -3, -2, 5, 6, -6];
+console.log(solution(arr3)); // -3 -2 -6 1 2 3 5 6
