e62a70fbf777f811f3113fb45a70044305ad71fc
Ella Yeonsu Choi <happymondaysu@gmail.com>
2024-03-21 12:01:41 +0900

    docs: Update 1. 재귀함수.js of Section 8

--- Inflearn/섹션8/1. 재귀함수.js
+++ Inflearn/섹션8/1. 재귀함수.js
@@ -1,12 +1,12 @@
 function solution(n){
-  function DFS(L){
-    if(L==0) return;
-    else{
-      DFS(L-1);
-      console.log(L);   
+  function DFS(L) {
+    if (L === 0) return;
+    else {
+      DFS(L - 1);
+      console.log;
     }
   }
   DFS(n);
 }
 
-solution(3);
+solution(3); // 1 2 3: 스택에서 pop되어 빠져나오는 순서
