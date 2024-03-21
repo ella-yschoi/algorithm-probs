e7e0f87604638820189fbf9ede0dbb399e33052e
Ella Yeonsu Choi <happymondaysu@gmail.com>
2024-03-21 12:16:23 +0900

    docs: Update 2. 이진수 출력(재귀).js of Section 8

--- Inflearn/섹션8/2. 이진수 출력(재귀).js
+++ Inflearn/섹션8/2. 이진수 출력(재귀).js
@@ -1,15 +1,16 @@
-function solution(n){
-  let answer="";
-  
-  function DFS(n){
-    if(n===0) return;
-    else{
-      DFS(parseInt(n/2));
-      answer+=String(n%2);
+function solution(n) {
+  let answer = '';
+
+  function DFS(n) {
+    if (n === 0) return;
+    else {
+      DFS(parseInt(n / 2)); // 몫
+      answer += String(n % 2); // 나머지
     }
   }
   DFS(n);
   return answer;
 }
 
-console.log(solution(11));
+console.log(solution(11)); // 1011
+
