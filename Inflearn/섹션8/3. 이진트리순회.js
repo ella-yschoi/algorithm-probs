dd3398d9a94d0629bb679ccec015dff8d6d1c6fc
Ella Yeonsu Choi <happymondaysu@gmail.com>
2024-03-21 15:30:36 +0900

    docs: Update 3. 이진트리순회.js of Section 8

--- Inflearn/섹션8/3. 이진트리순회.js
+++ Inflearn/섹션8/3. 이진트리순회.js
@@ -1,16 +1,17 @@
-function solution(n){
-  let answer="";
+function solution(n) {
+  let answer = "";
 
-  function DFS(v){
-    if(v>7) return;
-    else{
-      answer+=(v+' ');
-      DFS(v*2);
-      DFS(v*2+1);
+  function DFS(v) {
+    if (v > 7) return;
+    else {
+      answer += (v + ' '); // 전위 순회 1 2 4 5 3 6 7 
+      DFS(v * 2); // 왼쪽 자식
+      // answer += (v + ' '); // 중위 순회 4 2 5 1 6 3 7
+      DFS(v * 2 + 1); // 오른쪽 자식
+      // answer += (v + ' '); // 후위 순회 4 5 2 6 7 3 1 
     }
   }
   DFS(n);
-  
   return answer;
 }
 
