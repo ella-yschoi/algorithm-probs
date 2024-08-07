1844cb44026f2edf754ef7b28a3187c9710a4d7b
ella-yschoi <happymondaysu@gmail.com>
2024-08-07 14:25:18 +0900

    docs: Update 26_모의고사.js of Programmers

--- Programmers/Level.1/26_모의고사.js
+++ Programmers/Level.1/26_모의고사.js
@@ -71,3 +71,27 @@ function solution(answers) {
   // 결과 배열 반환
   return result;
 }
+
+// 다른 풀이 2
+function solution(answers) {
+  let answer = [];
+
+  const man1 = [1, 2, 3, 4, 5];
+  const man2 = [2, 1, 2, 3, 2, 4, 2, 5];
+  const man3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+
+  let count = [0, 0, 0];
+
+  for (let i = 0; i < answers.length; i++) {
+    if (answers[i] == man1[i % man1.length]) count[0]++;
+    if (answers[i] == man2[i % man2.length]) count[1]++;
+    if (answers[i] == man3[i % man3.length]) count[2]++;
+  }
+
+  const max = Math.max(count[0], count[1], count[2]);
+  for (let i = 0; i < count.length; i++) {
+    if (max == count[i]) answer.push(i + 1);
+  }
+
+  return answer;
+}
